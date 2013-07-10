da6a8e5bbb2ee6bb5c20970d6b8f9c4f12b49367
Steve Burns <steve@grog.com>
2013-07-10 22:23:26 +0000

    allowing -u option for url as per hw3part3

--- grader.js
+++ grader.js
@@ -24,7 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "https://raw.github.com/Grogoyle/bitstarter/master/index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,23 +39,25 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+    $ = cheerio.load(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+};
+
+var marketResearch = function(url, csvfile, headers) {
+    var response2console = buildfn(csvfile, headers);
+    rest.get(url).on('complete', response2console);
 };
 
 var clone = function(fn) {
@@ -63,12 +68,27 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), "")
+        .option('-u, --url <url_address>', 'URL to html file', clone(assertFileExists), HTMLURL_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var doFile = false;
+    if (program.file != "") doFile = true;
+    if (doFile) {
+        // load html from file
+        var html = fs.readFileSync(program.file);
+        checkHtml(html, program.checks);
+    } else {
+        // load html from url
+        rest.get(program.url).on('complete', function(result, response) {
+            if (result instanceof Error) {
+                console.error('Error: ' + util.format(response.message));
+            } else {
+                // console.log(response.raw.toString());
+                checkHtml(response.raw.toString(), program.checks);
+            }
+        });
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
